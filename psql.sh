f74006e3e7e6ae01f21fa34079e25f443be01aaf
mamachanko <sugardubz@gmail.com>
2020-01-07 15:24:24 +0100

    initial commit

---
+++ psql.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+./shell.sh \
+    psql \
+        --username postgres \
+        $@
---
+++ shell.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+echo
+echo "finding db container ..."
+DB_CONTAINER=$(docker-compose ps -q db)
+
+if [ $# -eq 0 ]; then
+    COMMAND=bash
+else
+    COMMAND=$@
+fi
+
+echo 
+echo running
+echo
+echo "  > $COMMAND"
+echo
+
+docker \
+    exec \
+    --interactive \
+    --tty \
+    --workdir /root \
+    "$DB_CONTAINER" \
+    $COMMAND
+ 
